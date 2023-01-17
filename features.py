b71c435253330d39862201438c86090d0cb7f217
Henmachado <henriqe_m@hotmail.com>
2023-01-16 22:16:55 -0300

    refactoring feature function

--- features.py
+++ features.py
@@ -1,21 +1,9 @@
-import pyspark.sql.functions as F
-
 from pyspark.sql import DataFrame
-from pyspark.sql import Window
 
 
-def create_sum_features_type_ever(
-        df: DataFrame, feature_list: list, sum_column: str
-) -> DataFrame:
-    _df = df
+def create_sum_features_type_ever(df: DataFrame, feature_list: list, sum_column: str) -> DataFrame:
     for feature in feature_list:
-        window = (
-            Window()
-            .partitionBy(feature)
-            .rowsBetween(Window.unboundedPreceding, Window.currentRow)
+        return df.selectExpr(
+            "*",
+            f"sum({sum_column}) over(partition by {feature}) as ft_sum_total_{feature}_ever"
         )
-        _df = df.withColumn(
-            f"ft_sum_total_{feature}_ever",
-            F.sum(F.col(sum_column)).over(window)
-        )
-    return _df
