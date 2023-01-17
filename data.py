c45739ba592ecfdd7648170d1adaadd55296f936
Henmachado <henriqe_m@hotmail.com>
2023-01-16 23:36:16 -0300

    minor fix

--- data.py
+++ data.py
@@ -9,18 +9,19 @@ schema = StructType([
     StructField("player_name", StringType()),
     StructField("team_name", StringType()),
     StructField("player_points", IntegerType()),
+    StructField("player_assists", IntegerType()),
 ])
 data = [
-    ('James', "Lakers", 28),
-    ('Irving', "Nets", 23),
-    ('Durant', "Nets", 35),
-    ('Curry', "Warriors", 38),
-    ('Harden', "Nets", 19)
+    ('James', "Lakers", 28, 14),
+    ('Irving', "Nets", 23, 13),
+    ('Durant', "Nets", 35, 8),
+    ('Curry', "Warriors", 38, 9),
+    ('Harden', "Nets", 19, 12)
     ]
 
 spark_df = spark.createDataFrame(data=data, schema=schema)
 
-dff = create_sum_features_type_ever(spark_df, ["team_name"], "player_points")
+dff = create_sum_features_type_ever(spark_df, ["team_name"], ["player_points", "player_assists"])
 
 dff.printSchema()
 dff.show(truncate=False)
--- features.py
+++ features.py
@@ -1,21 +1,17 @@
+import itertools
 import pyspark.sql.functions as F
 
 from pyspark.sql import DataFrame
 from pyspark.sql import Window
 
 
-def create_sum_features_type_ever(
-        df: DataFrame, feature_list: list, sum_column: str
-) -> DataFrame:
+def create_sum_features_type_ever(df: DataFrame, feature_list: list, sum_columns: list) -> DataFrame:
     _df = df
-    for feature in feature_list:
+    for feature, sum_column in itertools.product(feature_list, sum_columns):
         window = (
-            Window()
-            .partitionBy(feature)
-            .rowsBetween(Window.unboundedPreceding, Window.currentRow)
+            Window().partitionBy(feature).rowsBetween(Window.unboundedPreceding, Window.currentRow)
         )
-        _df = df.withColumn(
-            f"ft_sum_total_{feature}_ever",
-            F.sum(F.col(sum_column)).over(window)
+        _df = _df.withColumn(
+            f"ft_sum_total_{sum_column}_per_{feature}_ever", F.sum(F.col(sum_column)).over(window)
         )
     return _df
