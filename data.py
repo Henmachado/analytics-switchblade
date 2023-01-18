b3b8397fd155a1647ac3b8b69c45b012cbb63b08
Henmachado <henriqe_m@hotmail.com>
2023-01-17 22:08:04 -0300

    refactor features functions

--- data.py
+++ data.py
@@ -1,7 +1,9 @@
-from features import create_sum_features_type_ever
+import datetime
+
+from features import *
 
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DateType
 
 spark = SparkSession.builder.getOrCreate()
 
@@ -10,18 +12,23 @@ schema = StructType([
     StructField("team_name", StringType()),
     StructField("player_points", IntegerType()),
     StructField("player_assists", IntegerType()),
+    StructField("date", DateType()),
 ])
 data = [
-    ('James', "Lakers", 28, 14),
-    ('Irving', "Nets", 23, 13),
-    ('Durant', "Nets", 35, 8),
-    ('Curry', "Warriors", 38, 9),
-    ('Harden', "Nets", 19, 12)
+    ('James', "Lakers", 28, 14, datetime.datetime.strptime('2021-01-01', "%Y-%m-%d").date()),
+    ('Irving', "Nets", 23, 13, datetime.datetime.strptime('2021-01-01', "%Y-%m-%d").date()),
+    ('Durant', "Nets", 35, 8, datetime.datetime.strptime('2021-01-02', "%Y-%m-%d").date()),
+    ('Curry', "Warriors", 38, 9, datetime.datetime.strptime('2021-01-01', "%Y-%m-%d").date()),
+    ('Harden', "Nets", 19, 12, datetime.datetime.strptime('2021-01-03', "%Y-%m-%d").date())
     ]
 
 spark_df = spark.createDataFrame(data=data, schema=schema)
 
-dff = create_sum_features_type_ever(spark_df, ["team_name"], ["player_points", "player_assists"])
+dff = create_sum_feature(
+    spark_df,
+    create_window_frame_hour_interval(partition_key="team_name", order_key="date", interval=24),
+    sum_column="player_assists"
+)
 
 dff.printSchema()
 dff.show(truncate=False)
--- features.py
+++ features.py
@@ -1,17 +1,42 @@
-import itertools
 import pyspark.sql.functions as F
 
-from pyspark.sql import DataFrame
+from pyspark.sql import DataFrame, WindowSpec
 from pyspark.sql import Window
+from typing import Callable
 
 
-def create_sum_features_type_ever(df: DataFrame, feature_list: list, sum_columns: list) -> DataFrame:
+def create_window_frame_ever(partition_key: str, order_key: str):
+    window = (
+        Window()
+        .partitionBy(partition_key)
+        .orderBy(order_key)
+        .rowsBetween(Window.unboundedPreceding, Window.currentRow)
+    )
+    return window, f"{partition_key}_ever"
+
+
+def create_window_frame_hour_interval(partition_key: str, order_key: str, interval: int):
+    def to_hours(h): return h * 3600
+    window = (
+        Window()
+        .partitionBy(partition_key)
+        .orderBy(F.col(order_key).cast("timestamp").cast("long"))
+        .rangeBetween(-to_hours(interval), Window.currentRow)
+    )
+    return window, f"{partition_key}_last_{interval}h"
+
+
+def create_count_feature(
+        df: DataFrame, window: Callable[[str], tuple[WindowSpec, str]], count_column: str) -> DataFrame:
     _df = df
-    for feature, sum_column in itertools.product(feature_list, sum_columns):
-        window = (
-            Window().partitionBy(feature).rowsBetween(Window.unboundedPreceding, Window.currentRow)
-        )
-        _df = _df.withColumn(
-            f"ft_sum_total_{sum_column}_per_{feature}_ever", F.sum(F.col(sum_column)).over(window)
-        )
-    return _df
+    return _df.withColumn(
+        f"ft_total_count_{count_column}_per_{window[1]}", F.sum(F.lit(1)).over(window[0])
+    )
+
+
+def create_sum_feature(
+        df: DataFrame, window: Callable[[str], tuple[WindowSpec, str]], sum_column: str) -> DataFrame:
+    _df = df
+    return _df.withColumn(
+        f"ft_total_sum_{sum_column}_per_{window[1]}", F.sum(F.col(sum_column)).over(window[0])
+    )
