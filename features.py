edcf0808162ae58a6382f27dc21e9387d414495b
Henmachado <henriqe_m@hotmail.com>
2023-01-28 19:18:04 -0300

    minor fix

--- features.py
+++ features.py
@@ -3,32 +3,28 @@ from pyspark.sql import functions as F
 from pyspark.sql import Window
 
 
-def to_hours(h):
-    return h * 3600
-
-
-def generate_window_object(specs: tuple) -> tuple:
-    w = (
+def generate_window_spec(specs: tuple) -> tuple:
+    window_spec = (
         Window()
         .partitionBy(specs[0])
         .orderBy(F.col(specs[2]).cast("timestamp").cast("long"))
-        .rangeBetween(-to_hours(specs[1]), Window.currentRow)
+        .rangeBetween(-specs[1] * 3600, Window.currentRow)
     )
-    ft_name = f"per_{specs[0]}_{specs[1]}h_orderby_{specs[2]}"
-    return w, ft_name
+    feature_name = f"per_{specs[0]}_{specs[1]}h_orderby_{specs[2]}"
+    return window_spec, feature_name
 
 
-def generate_window_objects_list(specs: list[tuple]) -> list:
-    _wf = []
+def generate_window_spec_list(specs: list[tuple]) -> list:
+    window_spec_list = []
     for spec in specs:
-        w = generate_window_object(spec)
-        _wf.append(w)
-    return _wf
+        w = generate_window_spec(spec)
+        window_spec_list.append(w)
+    return window_spec_list
 
 
 def generate_count_feature(df: DataFrame, target_colum: str, specs: list[tuple]) -> DataFrame:
     _df = df
-    for spec in generate_window_objects_list(specs):
+    for spec in generate_window_spec_list(specs):
         _df = _df.withColumn(
             f"count_{target_colum}_{spec[1]}", F.count(target_colum).over(spec[0])
         )
@@ -37,7 +33,7 @@ def generate_count_feature(df: DataFrame, target_colum: str, specs: list[tuple])
 
 def generate_sum_feature(df: DataFrame, target_colum: str, specs: list[tuple]) -> DataFrame:
     _df = df
-    for spec in generate_window_objects_list(specs):
+    for spec in generate_window_spec_list(specs):
         _df = _df.withColumn(
             f"sum_{target_colum}_{spec[1]}", F.sum(target_colum).over(spec[0])
         )
